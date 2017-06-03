76534cf7c49c3e1a8dce73abb2b5873e88f88db8
religofsil <religionofsilence@gmail.com>
2017-06-03 13:55:39 +0300

    update files

---
+++ play.sh
@@ -0,0 +1,2 @@
+source activate myenv
+python /home/u3828/quorabot/translate.py  --en_vocab_size=40000 --fr_vocab_size=40000 --data_dir=/home/u3828/quorabot/train --train_dir=/home/u3828/quorabot/train
