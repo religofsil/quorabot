750d5a3d99404b6a03a694d75ca8196b07274043
Alexandra Ershova <religionofsilence@gmail.com>
2017-06-05 20:31:23 +0300

    gcloud compatibility

--- translate.py
+++ translate.py
@@ -42,8 +42,8 @@ import numpy as np
 from six.moves import xrange  # pylint: disable=redefined-builtin
 import tensorflow as tf
 
-from quorabot import data_utils
-from quorabot import seq2seq_model
+quorabot.data_utils
+quorabot.seq2seq_model
 
 
 tf.app.flags.DEFINE_float("learning_rate", 0.5, "Learning rate.")
